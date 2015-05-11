91d9b892bd351e3d8a2d4efbf4a71e46e714690b
erics <erics@localhost>
2015-05-11 16:42:30 -0400

    initial commit

---
+++ startprocess.sh
@@ -0,0 +1,1 @@
+curl -X POST -H 'Accept: application/json' -uerics 'localhost:8080/business-central/rest/runtime/com.redhat.bpms.examples:CaseManagement:1.0/process/com.redhat.bpms.examples.casemanagement.CaseManagementProcess/start?map_reporter=erics&map_location=work&map_description=description&map_isFullInvestigationString=true'
